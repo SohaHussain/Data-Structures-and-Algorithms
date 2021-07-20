3a3b54bd038374d7d5c00695793f6e374080c9bc
Soha <soha00hussain@gmail.com>
2021-07-20 18:14:45 +0530

    stocks- buy,sell

---
+++ Arrays/array5.cpp
@@ -0,0 +1,44 @@
+// stocks- buy, sell
+// input- an array of stock prices on each day
+// output- maximum profit
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int profit(int a[],int n)
+{
+    if(n==1)
+    return 0;
+    int i=0;
+    int profit=0;
+    while(i<n-1)
+    {
+        // local minimum
+        while((i<n-1)&& (a[i+1]<=a[i]))
+        i++;
+        if(i==n-1)
+        break;
+        int buy=i++;
+
+        // local maximum 
+        while((i<n)&&(a[i]>=a[i-1]))
+        i++;
+        int sell=i-1;
+
+        // updating profit 
+        profit+=a[sell]-a[buy];
+
+    }
+    return profit;
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    cout<<profit(a,n);
+    return 0;
+}
