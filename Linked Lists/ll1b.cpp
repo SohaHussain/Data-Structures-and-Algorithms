d6ece8e9f3fc3e775853fd73e1832cee427939e8
Soha <soha00hussain@gmail.com>
2021-09-03 00:20:16 +0530

    traversal

---
+++ Linked Lists/ll1b.cpp
@@ -0,0 +1,37 @@
+// traversal in a linked list 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// self referential structure used because every node has to have the address of the next node and data type of next node
+// is same as the current node
+
+struct node{
+    int data;
+    node *next;
+    node(int x)  // constructor
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    node *curr=head;
+    while(curr!=NULL)
+    {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+int main()
+{
+   node *head = new node(10);
+   head->next=new node(20);
+   head->next->next=new node(30);
+   head->next->next->next=new node(40);
+   printlist(head);
+   return 0;
+}
