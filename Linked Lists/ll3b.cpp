51a8b337014e0ebea396275ec1c730d185fda437
Soha <soha00hussain@gmail.com>
2021-09-05 01:07:43 +0530

    insertion at beginning

---
+++ Linked Lists/ll3b.cpp
@@ -0,0 +1,45 @@
+// insertion at the beginning of a singly linked list 
+// t -> O(n)
+// s -> O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// self referential structure used because every node has to have the address of the next node and data type of next node
+// is same as the current node
+
+struct node{
+    int data;
+    node *next;
+    node(int x)  // constructor
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+
+node *insertbegin(node *head, int x)
+{
+    node *temp = new node(x);
+    temp->next=head;
+    return temp;
+
+}
+void printlist(node *head)
+{
+    node *curr=head;
+    if(curr==NULL)
+    return;
+    cout<<curr->data<<" ";
+    printlist(curr->next);
+}
+
+int main()
+{
+   node *head = NULL;
+   head = insertbegin(head,10);
+   head = insertbegin(head,20);
+   printlist(head);
+   return 0;
+}
