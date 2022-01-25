ab8f4dbf126b0277e76822a4c2e1203137c701eb
Soha Hussain <soha00hussain@gmail.com>
2022-01-25 20:48:35 +0530

    selection sort

--- Sorting/sort2.cpp
+++ Sorting/sort2.cpp
@@ -1,24 +1,24 @@
 // Selection sort
-// In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray 
-// is picked and moved to the sorted subarray. 
 
 #include<bits/stdc++.h>
 using namespace std;
 
 void selsort(int a[],int n)
 {
-    int minin;
-    for(int i=0;i<n-1;i++)
+    for(int i=0;i<n;i++)
     {
-        minin=i;
-        for(int j=i+1;j<n;j++)
+        int maxeleindex=0;
+        int last = n-i-1;
+        for(int j=0;j<=last;j++)
         {
-            if(a[j]<a[minin])
-            minin=j;
+            if(a[j]>=a[maxeleindex])
+            maxeleindex=j;     
         }
-        swap(a[i],a[minin]);
+        swap(a[maxeleindex],a[last]);
     }
+    
 }
+
 int main()
 {
     int n;
