98da8f262908af3fba204ad823c1e3fdc11e579e
Soha Hussain <soha00hussain@gmail.com>
2022-02-08 19:28:36 +0530

    nth magic number

---
+++ Bit Magic/bit2.cpp
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/find-nth-magic-number/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n = 6;
+  int base = 5;
+  int ans = 0;
+  while(n>0)
+  {
+    int last = n&1;
+    n = n>>1;
+    ans += last*base;
+    base *=5;
+  }
+  cout<<ans;
+  return 0;
+}
+    
+    
