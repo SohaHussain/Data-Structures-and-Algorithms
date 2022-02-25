3d0f9e73e5f29e4f19a31caec7c2cb41c6ef3848
Soha Hussain <soha00hussain@gmail.com>
2022-02-25 20:10:38 +0530

    factors optimised

---
+++ Maths/maths3.cpp
@@ -0,0 +1,31 @@
+//https://www.geeksforgeeks.org/program-for-newton-raphson-method/
+
+// TC : O(sqrt(n))
+// SC : O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> fact;
+    int n;
+    cin>>n;
+    for(int i=1;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            if((n/i)==i)
+            fact.push_back(i);
+            else
+            {
+                fact.push_back(i);
+                fact.push_back(n/i);
+            }
+        }
+    }
+    sort(fact.begin(),fact.end());
+    for(auto i:fact)
+    cout<<i<<" ";
+    return 0;
+}
