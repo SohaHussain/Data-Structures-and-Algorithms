d4995b3af05dbf018d0067fe9a66dd1033b7d9c3
Soha Hussain <soha00hussain@gmail.com>
2022-06-19 16:46:57 +0530

    reverse a linked list - iterative

---
+++ Linked Lists/ll6.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+// iterative solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
+            return head;
+        ListNode *p=NULL;
+        ListNode *c=head;
+        ListNode *n=c->next;
+        while(c!=NULL)
+        {
+            c->next = p;
+            p=c;
+            c=n;
+            if(n!=NULL)
+                n=n->next;
+        }
+        return p;
+    }
+};
