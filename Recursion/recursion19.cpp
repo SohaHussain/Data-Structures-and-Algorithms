e93e9cea72b0eefdda61886475ae1ea913790476
Soha Hussain <soha00hussain@gmail.com>
2022-06-22 17:57:19 +0530

    printing all subsequences

---
+++ Recursion/recursion19.cpp
@@ -0,0 +1,27 @@
+// printing all the subsequences
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int ind, vector<int> &ans, int arr[], int n)
+{
+    if(ind == n)
+    {
+        for(auto i:ans)
+        cout<<i;
+        cout<<'\n';
+        return;
+    }
+    ans.push_back(arr[ind]);
+    solve(ind+1,ans,arr,n);
+    ans.pop_back();
+    solve(ind+1,ans,arr,n);
+}
+int main()
+{
+    int arr[]={3,1,2};
+    int n=3;
+    vector<int>ans;
+    solve(0,ans,arr,n);
+    return 0;
+}
