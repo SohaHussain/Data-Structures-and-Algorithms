6a7177794030d5043ed1a1ccac7d39c9ca52c752
Soha Hussain <soha00hussain@gmail.com>
2022-07-03 22:07:43 +0530

    Kth Largest Element in an Unsorted Array

--- Heap/1.cpp
+++ Heap/1.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 // TC : nlog(k)
+
 int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int,vector<int>,greater<int>> minh;
         for(int i=0;i<k;i++)
