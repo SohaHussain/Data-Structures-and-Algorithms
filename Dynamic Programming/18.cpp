a8b1da772a6ba399199d4927662bf9b60a2d5146
Soha Hussain <soha00hussain@gmail.com>
2022-07-21 16:36:21 +0530

    Min # deletions to make string palindromic

---
+++ Dynamic Programming/18.cpp
@@ -0,0 +1,32 @@
+// https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions4610/1
+
+int minDeletions(string str, int n) 
+{ 
+    string str2="";
+    int i=n-1;
+    while(i>=0)
+    {
+        str2+=str[i];
+        i--;
+    }
+    int dp[n+1][n+1];
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=n;j++)
+        {
+            if(i==0 || j==0)
+            dp[i][j]=0;
+            else if(str[i-1]==str2[j-1])
+            dp[i][j]=1+dp[i-1][j-1];
+            else
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    int lcs=INT_MIN;
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=n;j++)
+        lcs=max(lcs,dp[i][j]);
+    }
+    return (n-lcs);
+} 
