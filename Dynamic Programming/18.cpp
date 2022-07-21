b51453b7d0ff1dad12a22ddf04247d5d2fcdabd4
Soha Hussain <soha00hussain@gmail.com>
2022-07-21 16:45:59 +0530

    min # of del to make string palindromic(same for insertions)

--- Dynamic Programming/18.cpp
+++ Dynamic Programming/18.cpp
@@ -1,5 +1,7 @@
 // https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions4610/1
 
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
 int minDeletions(string str, int n) 
 { 
     string str2="";
