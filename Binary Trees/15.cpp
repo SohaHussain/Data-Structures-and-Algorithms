42b1b0e74071558f561597ca809b383343abce31
Soha Hussain <soha00hussain@gmail.com>
2023-10-27 16:58:32 +0530

    BT Level order treversal

---
+++ Binary Trees/15.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/?envType=list&envId=pxzfn47j
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)
+        return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int s=q.size();
+            vector<int> level;
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left!=NULL)
+                q.push(node->left);
+                if(node->right!=NULL)
+                q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
